af36456914cd7e7b9baa6b96030b1ebdae983bcf
SourCream <>
2021-04-05 19:30:51 +0000

    testing a new function

--- src/Userinfo.js
+++ src/Userinfo.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch').default;
 
 module.exports={
-async coins(url,token,userid,message){
+async coins(url,token,userid){
   if(!url) throw new TypeError("No url was specified")
   if(!userid) throw new TypeError("No user id was specified")
   if(!token) throw new TypeError("No token was provided")
-  if(!message) throw new TypeError("No message (message.channel) was provided")
+  //if(!message) throw new TypeError("No message (message.channel) was provided")
 
-  fetch(`${url}/api/userinfo/?id=${userid}`, {
+  const got = await fetch(`${url}/api/userinfo/?id=${userid}`, {
       method: 'GET',
       headers: { 
         'Content-Type': 'application/json',
@@ -16,9 +16,8 @@ async coins(url,token,userid,message){
       body:    null,
   })
   .then(res => res.json())
-  .then(async (json) => {
-      return message.reply(json.coins);
-  });
+  .then(json => (json.coins))
+  .then(return got)
     //.then(json => (json.coins))
     //.then(res => console.log(res));
     //.then(res => res.text())

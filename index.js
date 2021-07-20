86f62407eb13f3bb24f484380525150e055d0056
SourCreamCulture <73759064+SourCreamCulture@users.noreply.github.com>
2021-07-19 23:09:18 -0400

    testing
    testing new stuff

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  Userinfo: require('./src/Userinfo'),
-  Package: require('./src/Package'),
-  Extra: require('./src/Extra'),
-  Plan: require('./src/Plan'),
-  version: require('./package.json').version
-}
